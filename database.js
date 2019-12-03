99b4a10e62240c0b057301c5bb056c096a435427
UtkarshMish <iiutkarshmishraii@gmail.com>
2019-12-03 22:31:01 +0530

    removed password authentications

--- database.js
+++ database.js
@@ -1,6 +1,7 @@
+const { USERNAME, PASSWORD } = require("./.env");
 const mongoose = require("mongoose");
 module.exports = mongoose.connect(
-  "mongodb+srv://utkarsh_mishra:ut@sH4545@cluster0-xby9y.mongodb.net/leadManager?retryWrites=true",
+  `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0-xby9y.mongodb.net/leadManager?retryWrites=true`,
   { useNewUrlParser: true, useCreateIndex: true },
   function(err, db) {
     if (err) throw err;
